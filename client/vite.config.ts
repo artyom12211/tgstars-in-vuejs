b9ed15814789123954fdf8e8e1138abf06ce70c8
Art <feelgrime@mail.ru>
2025-02-16 21:36:03 +0300

    feat: exposed https connection for client

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,6 +4,12 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
+import fs from 'fs';
+import path from 'path';
+
+const keyPath = path.resolve(__dirname, 'certs', 'key.pem');
+const certPath = path.resolve(__dirname, 'certs', 'cert.pem');
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -14,5 +20,15 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+
+  server: {
+    hmr: true,
+    host: '0.0.0.0', // Liisten all LAN addresses
+    https: {
+      key:  fs.readFileSync(keyPath),
+      cert: fs.readFileSync(certPath),
+    },
+    port: 8080,
+  },
 })
